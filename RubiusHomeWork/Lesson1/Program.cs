d96679c69208aa3592970d1ef0cdf31eb9e8d978
Mark Rikhter <markrihter98@gmail.com>
2024-10-26 23:59:17 +0700

    initial homework 1 commit

---
+++ RubiusHomeWork/Lesson1/Program.cs
@@ -0,0 +1,6 @@
+﻿const string greetingString = """
+                              Hi! I a'm Rikhter Mark.
+                              I am 26.
+                              """;
+
+Console.WriteLine(greetingString);
